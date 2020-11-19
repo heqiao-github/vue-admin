f6998e8d0288cbe5bb3224c0b0d55623626b8f99
yourname <your@email.com>
2020-11-19 19:00:10 +0800

    3333

---
+++ .history/src/router/index_20201119180848.js
@@ -0,0 +1,169 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: 'baseCaseInfo',//'/dashboard',
+    meta:{title:'案件管理'},
+    children: [
+      {
+      path: 'baseCaseInfo',//'dashboard',
+      name: 'baseCaseInfo',
+      component: () => import('@/views/caseManage/baseCaseInfo'),
+      meta: { title: '基本案情',affix:true }
+    },{
+      path:"gangInfo",
+      name:'gangInfo',
+      component:() => import('@/views/caseManage/gangInfo') ,
+      meta:{title: '团伙信息'}
+    },{
+      path:"personStructure",
+      name:'personStructure',
+      component:() => import('@/views/caseManage/personStructure') ,
+      meta:{title: '层级架构'}
+    },{
+      path:"resultLog",
+      name:'resultLog',
+      component:() => import('@/views/caseManage/resultLog') ,
+      meta:{title: '研判日志'}
+    },{
+      path:"suspectInfo",
+      name:'suspectInfo',
+      component:() => import('@/views/caseManage/suspectInfo') ,
+      meta:{title: '涉案人信息'}
+    },{
+      path:"waitingList",
+      name:'waitingList',
+      component:() => import('@/views/caseManage/waitingList') ,
+      meta:{title: '待调单清单'}
+    }
+  ]
+  },
+
+  {
+    path: '/dataImport',
+    component: Layout,
+    redirect: '/dataImport/dataInStorage',
+    name: 'dataImport',
+    meta: { title: '数据导入' },
+    children: [
+      {
+        path: 'dataInStorage',
+        name: 'dataInStorage',
+        component: () => import('@/views/dataImport/dataInStorage'),
+        meta: { title: '数据入库', icon: 'table' }
+      },
+      {
+        path: 'dataClean',
+        name: 'dataClean',
+        component: () => import('@/views/dataImport/dataClean'),
+        meta: { title: '数据清洗', icon: 'tree' }
+      },
+      {
+        path: 'personTable',
+        name: 'dataClean',
+        component: () => import('@/views/dataImport/personList'),
+        meta: { title: '调单人员表' }
+      }
+    ]
+  },
+  
+
+  {
+    path: '/personList',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
